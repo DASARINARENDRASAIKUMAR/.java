773ea03b41264daeeae3a603b94e2cd06e858564
DASARINARENDRASAIKUMAR <saikumardasari9346@gmail.com>
2025-06-18 12:30:35 +0530

    Create jumblednumpattern.java

---
+++ jumblednumpattern.java
@@ -0,0 +1,13 @@
+class NumberPrinter{
+public static void main(String []args){
+int n=1;
+for(int i=1;i<=5;i++){
+for(int j=0;j<5;j++)
+{
+System.out.print(n + " ");
+n=n+2;
+}
+System.out.println();
+}
+}
+}
